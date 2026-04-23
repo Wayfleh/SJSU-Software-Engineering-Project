2f96ea3cf845eb5696fdd11b5da849c70f78b211
Wayfleh <rafael.caculba04@gmail.com>
2026-04-22 20:03:54 -0700

    added things to database schema
    added foreign key for item author. column user_id INT references users(user_id)

---
+++ database/studenthub_backup_04-22-2026.sql
@@ -0,0 +1,369 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict bS0h10NpJrFAUkiuEIdaYFEKO0gyjvh28Ax9GodTAchiycvN66kpDEXrv18rFNu
+
+-- Dumped from database version 18.3
+-- Dumped by pg_dump version 18.3
+
+-- Started on 2026-04-22 20:01:25
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 859 (class 1247 OID 16386)
+-- Name: item_type; Type: TYPE; Schema: public; Owner: postgres
+--
+
+CREATE TYPE public.item_type AS ENUM (
+    'event',
+    'place',
+    'resource'
+);
+
+
+ALTER TYPE public.item_type OWNER TO postgres;
+
+--
+-- TOC entry 225 (class 1255 OID 16393)
+-- Name: update_updated_at(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.update_updated_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+	IF (NEW IS DISTINCT FROM OLD) THEN
+		NEW.updated_at = CURRENT_TIMESTAMP;
+	END IF;
+	RETURN NEW;
+END;
+$$;
+
+
+ALTER FUNCTION public.update_updated_at() OWNER TO postgres;
+
+--
+-- TOC entry 226 (class 1255 OID 16454)
+-- Name: update_verified_at(); Type: FUNCTION; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION public.update_verified_at() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+	IF (NEW IS DISTINCT FROM OLD) THEN
+		NEW.verified_at = CURRENT_TIMESTAMP;
+	END IF;
+	RETURN NEW;
+END;
+$$;
+
+
+ALTER FUNCTION public.update_verified_at() OWNER TO postgres;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 219 (class 1259 OID 16394)
+-- Name: items; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.items (
+    item_id integer NOT NULL,
+    item_name text NOT NULL,
+    item_desc text,
+    is_timed boolean,
+    timeframe text,
+    loc_is_iframe_url boolean,
+    loc_content text,
+    img_url text,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    user_id integer NOT NULL
+);
+
+
+ALTER TABLE public.items OWNER TO postgres;
+
+--
+-- TOC entry 220 (class 1259 OID 16403)
+-- Name: items_item_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.items ALTER COLUMN item_id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.items_item_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 221 (class 1259 OID 16404)
+-- Name: reviews; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.reviews (
+    review_id integer NOT NULL,
+    review_header text NOT NULL,
+    review_desc text NOT NULL,
+    user_id integer,
+    item_id integer,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.reviews OWNER TO postgres;
+
+--
+-- TOC entry 222 (class 1259 OID 16414)
+-- Name: reviews_review_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.reviews ALTER COLUMN review_id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.reviews_review_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 223 (class 1259 OID 16415)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    user_name text NOT NULL,
+    email text NOT NULL,
+    pfp_url text,
+    bio text,
+    verified_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    verification_token text,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 224 (class 1259 OID 16427)
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.users ALTER COLUMN user_id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.users_user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 5044 (class 0 OID 16394)
+-- Dependencies: 219
+-- Data for Name: items; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.items (item_id, item_name, item_desc, is_timed, timeframe, loc_is_iframe_url, loc_content, img_url, created_at, updated_at, user_id) FROM stdin;
+\.
+
+
+--
+-- TOC entry 5046 (class 0 OID 16404)
+-- Dependencies: 221
+-- Data for Name: reviews; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.reviews (review_id, review_header, review_desc, user_id, item_id, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- TOC entry 5048 (class 0 OID 16415)
+-- Dependencies: 223
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (user_id, user_name, email, pfp_url, bio, verified_at, verification_token, created_at, updated_at) FROM stdin;
+\.
+
+
+--
+-- TOC entry 5055 (class 0 OID 0)
+-- Dependencies: 220
+-- Name: items_item_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.items_item_id_seq', 1, true);
+
+
+--
+-- TOC entry 5056 (class 0 OID 0)
+-- Dependencies: 222
+-- Name: reviews_review_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.reviews_review_id_seq', 3, true);
+
+
+--
+-- TOC entry 5057 (class 0 OID 0)
+-- Dependencies: 224
+-- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_user_id_seq', 4, true);
+
+
+--
+-- TOC entry 4880 (class 2606 OID 16429)
+-- Name: items items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.items
+    ADD CONSTRAINT items_pkey PRIMARY KEY (item_id);
+
+
+--
+-- TOC entry 4882 (class 2606 OID 16431)
+-- Name: reviews reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT reviews_pkey PRIMARY KEY (review_id);
+
+
+--
+-- TOC entry 4885 (class 2606 OID 16433)
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- TOC entry 4887 (class 2606 OID 16435)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- TOC entry 4889 (class 2606 OID 16437)
+-- Name: users users_user_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_user_name_key UNIQUE (user_name);
+
+
+--
+-- TOC entry 4878 (class 1259 OID 16438)
+-- Name: item_index; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX item_index ON public.items USING btree (item_name);
+
+
+--
+-- TOC entry 4883 (class 1259 OID 16439)
+-- Name: user_index; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX user_index ON public.users USING btree (user_name);
+
+
+--
+-- TOC entry 4893 (class 2620 OID 16440)
+-- Name: items item_updated_at_trigger; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER item_updated_at_trigger BEFORE UPDATE ON public.items FOR EACH ROW EXECUTE FUNCTION public.update_updated_at();
+
+
+--
+-- TOC entry 4894 (class 2620 OID 16452)
+-- Name: reviews review_updated_at_trigger; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER review_updated_at_trigger BEFORE UPDATE ON public.reviews FOR EACH ROW EXECUTE FUNCTION public.update_updated_at();
+
+
+--
+-- TOC entry 4895 (class 2620 OID 16441)
+-- Name: users user_updated_at_trigger; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER user_updated_at_trigger BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.update_updated_at();
+
+
+--
+-- TOC entry 4896 (class 2620 OID 16455)
+-- Name: users user_verified_at_trigger; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER user_verified_at_trigger BEFORE UPDATE OF verification_token ON public.users FOR EACH ROW EXECUTE FUNCTION public.update_verified_at();
+
+
+--
+-- TOC entry 4891 (class 2606 OID 16456)
+-- Name: reviews fk_item; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES public.items(item_id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 4890 (class 2606 OID 24577)
+-- Name: items fk_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.items
+    ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
+
+--
+-- TOC entry 4892 (class 2606 OID 16447)
+-- Name: reviews fk_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
+
+-- Completed on 2026-04-22 20:01:26
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict bS0h10NpJrFAUkiuEIdaYFEKO0gyjvh28Ax9GodTAchiycvN66kpDEXrv18rFNu
+
