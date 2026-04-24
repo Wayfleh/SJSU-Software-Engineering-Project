a231407d5f400e73d76eac3d54b26d7f37b8c900
Prabhjot <prabhjotsingh@sjsu.edu>
2026-04-23 21:54:22 -0700

    Neon DB integration + auth + protected items/reviews working

--- backend/src/routes/reviewsRoutes.js
+++ backend/src/routes/reviewsRoutes.js
@@ -7,12 +7,6 @@ const {
 } = require("../controllers/reviewsController");
 
 router.get("/items/:id/reviews", getReviewsByItem);
-//router.post("/reviews", requireAuth, createReview);
-
-
-router.post("/reviews", (req, res, next) => {
-  req.user = { user_id: 5 };
-  next();
-}, createReview);
+router.post("/reviews", requireAuth, createReview);
 
 module.exports = router;
